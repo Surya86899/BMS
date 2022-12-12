9619e7108c763066c723db988f957c88fbeeb135
Surya86899 <vu4f2122034@pvppcoe.ac.in>
2022-12-12 10:36:52 +0530

    commit

--- MiniProject/src/Main.java
+++ MiniProject/src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        Wel w = new Wel();
+        Welcomepage we = new Welcomepage();
         //LoginForm1 Log = new  LoginForm1();
         //CreateAccount Cr = new CreateAccount();
         /*BackgroundImageJFrame Bg = new BackgroundImageJFrame();*/
         //Maintenance Maint = new Maintenance();
         //Complaint comp = new Complaint();
         //Events Even = new Events();
-        /*Profile Pro = new Profile();
-        Welcome Wel = new Welcome();
-        JDBC Jaa = new JDBC();*/
+        //Profile Pro = new Profile();
+        //Welcomepage Wel = new Welcomepage();
+        //JDBC Jaa = new JDBC();
         //about ab = new about();    
     }
 }
